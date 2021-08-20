b0be7d293c16296d089b5a87da37fd063e875083
kelskye <kelskye@gmail.com>
2021-08-20 14:09:23 +1000

    Fix the update function Clean up the logging Change which ID element is used as a row ID

--- src/main/java/net/fhirfactory/pegacorn/hestia/audit/dm/AuditEventProxy.java
+++ src/main/java/net/fhirfactory/pegacorn/hestia/audit/dm/AuditEventProxy.java
@@ -77,7 +77,6 @@ public class AuditEventProxy extends AuditBaseProxy {
     private static final byte[] Q_PURPOSE = Bytes.toBytes("PURPOSE");
     private static final byte[] Q_BODY = Bytes.toBytes("BODY");
 
-
     private int nextId;
 
     /**
@@ -94,64 +93,47 @@ public class AuditEventProxy extends AuditBaseProxy {
             Table table = connection.getTable(TABLE_NAME);
             Get g = new Get(Bytes.toBytes(theId.getIdPart()));
             Result result = table.get(g);
-            if(result.isEmpty()) {
+            if (result.isEmpty()) {
                 throw new ResourceNotFoundException(theId);
             }
             LOG.info("Result not empty. Size: " + result.size());
-          
-            byte [] data = result.getValue(CF2,Q_BODY);
+
+            byte[] data = result.getValue(CF2, Q_BODY);
             String json = Bytes.toString(data);
             AuditEvent audit = (AuditEvent) parseResourceFromJsonString(json);
-            
+
             return audit;
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
             throw new ResourceNotFoundException(theId);
         }
-       
+
     }
 
     @Create
     public MethodOutcome createEvent(@ResourceParam AuditEvent theEvent) {
-        
-        long time = Calendar.getInstance().getTimeInMillis();
-//        for(int i = 0; i < 1000; i++) {
-  
-        //TODO figure out how to get event ID
-        theEvent.setId("Audit-" + nextId++);
-            LOG.info("AuditEvent registered. ID: " + theEvent.getId());
-  
-            try {
-                saveToDatabase(theEvent);
-    //           writeToFileSystem(fileName, parsedResource);
-    
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-           
-//        }
-        // Inform the server of the ID for the newly stored resource
-
-        long totalTime = Calendar.getInstance().getTimeInMillis() - time;
-        LOG.info("Total time for 1 record (msec): " + totalTime );
-        
-        return new MethodOutcome().setId(theEvent.getIdElement());
-    }
-    
-    @Update()
-    public MethodOutcome updateEvent(@ResourceParam AuditEvent theEvent) {
-        LOG.debug(".updateEvent(): Entry, theEvent (AuditEvent) --> {}", theEvent);
-        
+        LOG.debug(".createEvent(): Entry, theEvent (AuditEvent) --> {}", theEvent);
+        // TODO figure out how to get event ID
+        theEvent.getIdElement().setId("Audit-" + nextId++);
         try {
             saveToDatabase(theEvent);
-//           writeToFileSystem(fileName, parsedResource);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        return new MethodOutcome().setId(theEvent.getIdElement());
+    }
+
+    @Update
+    public MethodOutcome updateEvent(@ResourceParam AuditEvent theEvent) {
+        LOG.debug(".updateEvent(): Entry, theEvent (AuditEvent) --> {}", theEvent);
+        try {
+            saveToDatabase(theEvent);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new MethodOutcome().setId(theEvent.getIdElement());
-
 
     }
 
@@ -161,14 +143,13 @@ public class AuditEventProxy extends AuditBaseProxy {
         throw (new UnsupportedOperationException("deleteEvent() is not supported"));
     }
 
-
     @Override
     protected void saveToDatabase(IDomainResource resource) {
         // TODO Auto-generated method stub
         try {
-           Connection connection = getConnection();
+            Connection connection = getConnection();
             createTable(connection.getAdmin());
-            saveData(connection, (AuditEvent)resource);
+            saveData(connection, (AuditEvent) resource);
         } catch (MasterNotRunningException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -183,104 +164,99 @@ public class AuditEventProxy extends AuditBaseProxy {
 
     private void saveData(Connection connection, AuditEvent resource) throws IOException {
         Table table = connection.getTable(TABLE_NAME);
-        Put row = new Put(Bytes.toBytes(resource.getId()));
-        
+        Put row = new Put(Bytes.toBytes(resource.getIdElement().getId()));
+
         addAgent(resource, row);
         addUpdateDate(resource, row);
         addPeriod(resource, row);
         addSource(resource, row);
         addPurposeOfEvent(resource, row);
-        row.addColumn(CF2, Q_BODY, 
-                Bytes.toBytes(parseResourceToJsonString(resource)));
+        row.addColumn(CF2, Q_BODY, Bytes.toBytes(parseResourceToJsonString(resource)));
         table.put(row);
-        LOG.info("Save successful. Id: " + Bytes.toString(row.getRow()));
+        LOG.debug("Save successful. Id: " + Bytes.toString(row.getRow()));
         table.close();
     }
 
-
     private void addAgent(AuditEvent resource, Put row) {
-        if(resource.getAgent() != null) {
-            for(AuditEventAgentComponent agent: resource.getAgent()) {
-                //Store the first name found
-                if(StringUtils.isNotBlank(agent.getName())) {
+        if (resource.getAgent() != null) {
+            for (AuditEventAgentComponent agent : resource.getAgent()) {
+                // Store the first name found
+                if (StringUtils.isNotBlank(agent.getName())) {
                     row.addColumn(CF1, Q_NAME, Bytes.toBytes(agent.getName()));
-                    LOG.info("Agent added: " + agent.getName());
+                    LOG.debug("Agent added: " + agent.getName());
                     break;
                 }
             }
         }
-        
+
     }
 
     private void addUpdateDate(AuditEvent resource, Put row) {
-       if(resource.getMeta() != null && resource.getMeta().getLastUpdated() != null) {
-           row.addColumn(CF1, Q_UPDATE, Bytes.toBytes(resource.getMeta().getLastUpdated().getTime()));
-           LOG.info("Update date added: " + resource.getMeta().getLastUpdated().toString());
-       }
+        if (resource.getMeta() != null && resource.getMeta().getLastUpdated() != null) {
+            row.addColumn(CF1, Q_UPDATE, Bytes.toBytes(resource.getMeta().getLastUpdated().getTime()));
+            LOG.debug("Update date added: " + resource.getMeta().getLastUpdated().toString());
+        }
     }
 
     private void addPeriod(AuditEvent resource, Put row) {
-       if(resource.getPeriod() != null) {
-           if(resource.getPeriod().getStart() != null) {
-               row.addColumn(CF1, Q_PSTART, Bytes.toBytes(resource.getPeriod().getStart().getTime()));
-               LOG.info("Pending start added: " + resource.getPeriod().getStart().toString());
+        if (resource.getPeriod() != null) {
+            if (resource.getPeriod().getStart() != null) {
+                row.addColumn(CF1, Q_PSTART, Bytes.toBytes(resource.getPeriod().getStart().getTime()));
+                LOG.debug("Pending start added: " + resource.getPeriod().getStart().toString());
 
-           }
-           if(resource.getPeriod().getEnd() != null) {
-               row.addColumn(CF1, Q_PEND, Bytes.toBytes(resource.getPeriod().getEnd().getTime()));
-               LOG.info("Pending end added: " + resource.getPeriod().getEnd().toString());
-           }
-       }
-    }
-
-    private void addSource(AuditEvent resource, Put row) {
-       if(resource.getSource() != null) {
-           StringBuilder sb = new StringBuilder();
-           //TODO is this correct?
-           for(Coding type : resource.getSource().getType()) {
-               sb.append(type.getCode());
-               sb.append(',');
-           }
-           if(sb.length() > 0) {
-               row.addColumn(CF1, Q_TYPE, Bytes.toBytes(sb.substring(0, sb.length() -1)));
-               LOG.info("Source added: " + sb.substring(0, sb.length() -1));
-           }
-           
-       }
-    }
-    
-    
-    private void addPurposeOfEvent(AuditEvent resource, Put row) {
-        if(resource.getPurposeOfEvent() != null) {
-            StringBuilder sb = new StringBuilder();
-            for(CodeableConcept purpose : resource.getPurposeOfEvent()) {
-                sb.append(purpose.getTextElement());
-                sb.append(',');
             }
-            if(sb.length() > 0) {
-                row.addColumn(CF1, Q_PURPOSE, Bytes.toBytes(sb.substring(0, sb.length() -1)));
-                LOG.info("Purpose added: " + sb.substring(0, sb.length() -1));
+            if (resource.getPeriod().getEnd() != null) {
+                row.addColumn(CF1, Q_PEND, Bytes.toBytes(resource.getPeriod().getEnd().getTime()));
+                LOG.debug("Pending end added: " + resource.getPeriod().getEnd().toString());
             }
         }
     }
-    
+
+    private void addSource(AuditEvent resource, Put row) {
+        if (resource.getSource() != null) {
+            StringBuilder sb = new StringBuilder();
+            // TODO is this correct?
+            for (Coding type : resource.getSource().getType()) {
+                sb.append(type.getCode());
+                sb.append(',');
+            }
+            if (sb.length() > 0) {
+                row.addColumn(CF1, Q_TYPE, Bytes.toBytes(sb.substring(0, sb.length() - 1)));
+                LOG.debug("Source added: " + sb.substring(0, sb.length() - 1));
+            }
+
+        }
+    }
+
+    private void addPurposeOfEvent(AuditEvent resource, Put row) {
+        if (resource.getPurposeOfEvent() != null) {
+            StringBuilder sb = new StringBuilder();
+            for (CodeableConcept purpose : resource.getPurposeOfEvent()) {
+                sb.append(purpose.getTextElement());
+                sb.append(',');
+            }
+            if (sb.length() > 0) {
+                row.addColumn(CF1, Q_PURPOSE, Bytes.toBytes(sb.substring(0, sb.length() - 1)));
+                LOG.debug("Purpose added: " + sb.substring(0, sb.length() - 1));
+            }
+        }
+    }
+
     private void createTable(Admin admin) throws IOException {
         if (!admin.tableExists(TABLE_NAME)) {
             TableDescriptorBuilder builder = TableDescriptorBuilder.newBuilder(TABLE_NAME);
             Collection<ColumnFamilyDescriptor> families = new ArrayList<ColumnFamilyDescriptor>();
             families.add(ColumnFamilyDescriptorBuilder.of(CF1));
             families.add(ColumnFamilyDescriptorBuilder.of(CF2));
-            builder.setColumnFamilies(families );
+            builder.setColumnFamilies(families);
             TableDescriptor desc = builder.build();
             admin.createTable(desc);
         }
     }
 
-
     @Override
     public Class<? extends IBaseResource> getResourceType() {
         return AuditEvent.class;
     }
-
 
 }
