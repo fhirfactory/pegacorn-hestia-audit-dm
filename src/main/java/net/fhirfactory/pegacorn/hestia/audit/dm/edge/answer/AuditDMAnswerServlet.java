efa3fbf1c866c466c702bb0dc1157ef83ee28fbb
kelskye <kelskye@gmail.com>
2021-09-03 14:53:49 +1000

    Cleaning up the DM and beginning to move the code to the Edge model used by Ladon.

---
+++ src/main/java/net/fhirfactory/pegacorn/hestia/audit/dm/edge/answer/AuditDMAnswerServlet.java
@@ -0,0 +1,14 @@
+package net.fhirfactory.pegacorn.hestia.audit.dm.edge.answer;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.slf4j.Logger;
+
+import ca.uhn.fhir.rest.server.RestfulServer;
+
+@ApplicationScoped
+public class AuditDMAnswerServlet  extends RestfulServer {
+
+    private static final long serialVersionUID = 1L;
+
+}
